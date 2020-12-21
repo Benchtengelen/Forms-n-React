bc1cb56548025a6ea00300cecdc3fe6c8f352159
Feyyaz Yılmaz <fyyz.ylmz.bjk@gmail.com>
2020-12-21 21:55:36 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'
+
+class App extends Component {
+ /* constructor() {
+    super()
+    this.state = {}
+    this.clickHandler = this.clickHandler.bind(this)
+
+    
+  } */
 
 
-class App extends Component {
-     constructor() {
-         super()
-         this.state = {
-            IsLoggedIn: false
-         }
-     }
+
+  state={
+    name: "Feyyaz Yılmaz",
+    state: "offline"
+  }
+
+  clickHandler = (e) => {
+    console.log('Butona tıklandı')
+    console.log(e.target)
+    this.setState({
+      state:'offline'
+    })
+  }
 
 
+  changeHandler = (e) => {
+   // console.log(e.target)
+    this.setState({
+      state: e.target.value
 
-    render() {
-        let wordDisplay
-        if (this.state.IsLoggedIn == true) {
-              wordDisplay = "in"
-        } else {
-            wordDisplay = "out"
-        }
-        return (
-            <div>
-                 <h1>You are currently logged {wordDisplay} </h1>
-            </div>
-        )
-    }
+    })
+
+  }
+  changeNameHandler = (e) => {
+    this.setState({
+    name: e.target.value
+
+    })
+
+  }
+
+ 
+
+  render() {
+    const {name, state} = this.state;
+    return (
+      <div>
+        <h1>Adı: {name}</h1>
+        <h3>Durum: {state}</h3>
+
+        <button onClick={this.clickHandler}>Durum değiştir</button>
+        
+        <select name="state" id="state" onChange={this.changeHandler} value={state}>
+          <option value="online">online</option>
+          <option value="offline">offline</option>
+          <option value="busy">busy</option>
+
+        </select>
+        <br/>
+       <input type="text" name="name" onChange={this.changeNameHandler}/>
+      </div>
+   
+    )
+  }
 }
 
-
-
-export default App
+export default App
